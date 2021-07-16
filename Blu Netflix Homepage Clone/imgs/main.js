191ef7a1f82ee409d5ae262b044eda0e3e3a2625
Neo <83064980+yadie7@users.noreply.github.com>
2021-07-16 18:27:40 +0530

    main

---
+++ Blu Netflix Homepage Clone/imgs/main.js
@@ -0,0 +1,28 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+
+function selectItem(e) {
+    removeBorder();
+    removeShow();
+   
+    this.classList.add('tab-border');
+    
+  
+    console.log(tabContentItem);
+   
+    tabContentItem.classList.add('show');
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+// click function hehe pheww
+tabItems.forEach(item => item.addEventListener('click', selectItem));
+
